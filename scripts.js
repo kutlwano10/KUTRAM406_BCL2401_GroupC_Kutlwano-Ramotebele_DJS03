9066f62e347264400488c6592e554f93fd2d2467
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-05-14 20:32:50 +0200

    Created a () that adds books to the html and display them

--- scripts.js
+++ scripts.js
@@ -5,25 +5,31 @@ let matches = books
 
 const starting = document.createDocumentFragment()
 
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
-
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `
-
-    starting.appendChild(element)
+const addBooksToHtml = () => {
+    for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
+        const element = document.createElement('button')
+        element.classList = 'preview'
+        element.setAttribute('data-preview', id)
+    
+        element.innerHTML = `
+            <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `
+    
+        starting.appendChild(element)
+    }
 }
+addBooksToHtml()
+
+
+
 
 document.querySelector('[data-list-items]').appendChild(starting)
 
